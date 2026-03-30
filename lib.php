61a3ea9ae0ddb6c4b34d751e8a2f3e67d562acff
Michael Dymund <Michael.Dymund@uregina.ca>
2026-03-30 16:23:33 -0600

    allows a user to see all of their Open Forum posts regardless of Group membership

--- lib.php
+++ lib.php
@@ -5373,10 +5373,10 @@ function hsuforum_user_can_see_timed_discussion($discussion, $user, $context) {
 * @param object $context
 * @return boolean returns true if they can view post, false otherwise
 */
-function hsuforum_user_can_see_group_discussion($discussion, $cm, $context) {
-
+function hsuforum_user_can_see_group_discussion($discussion, $cm, $context) {;
     // If it's a grouped discussion, make sure the user is a member.
-    if ($discussion->groupid > 0) {
+    // Unless they are viewing their own posts on their profile page, where we'd like them to see all of their posts regardless of their group
+    if ($discussion->groupid > 0 && basename($_SERVER['PHP_SELF']) != 'user.php') {
         $groupmode = groups_get_activity_groupmode($cm);
         if ($groupmode == SEPARATEGROUPS) {
             return groups_is_member($discussion->groupid) || has_capability('moodle/site:accessallgroups', $context);
